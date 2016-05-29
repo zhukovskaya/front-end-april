69b3a750d6d638fc824cde33d0e11862ee322e23
brainacadem <brainacadem@gmail.com>
2016-05-29 22:46:52 +0300

    Update arrays.js

--- tasks/06_javascript_base/arrays/arrays.js
+++ tasks/06_javascript_base/arrays/arrays.js
@@ -60,6 +60,7 @@ function getLastElements(array, n) {
 
 /*
  5. Write a simple JavaScript program to join all elements of the following array into a string.
+ Set second argument as choisen separator (e.g. - ";" , "," , "+"). By default must be ",".
  Sample array : myColor = ["Red", "Green", "White", "Black"];
  Expected Output :
  "Red,Green,White,Black"
