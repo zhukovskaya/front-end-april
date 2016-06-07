bd38d26a7801f0f258f4931ef35d6fa637698b51
Raienko <raienko.a@gmail.com>
2016-06-07 21:16:43 +0300

    refExp lesson

--- Raienko/jsCourse/lessons/01/js/magic.js
+++ Raienko/jsCourse/lessons/01/js/magic.js
@@ -1,16 +1,12 @@
-function promptChecker() {
-  var i=0;
-  var mark;
-  for (; i<5;i++) {
-    mark = prompt('Enter mark');
-    if (mark < 60) {
-      break;
-    } else {
-      console.log("Passed");
-    }
-  }
-}
-
-function promptCalculator() {
-// to do
+function stringMagic(string) {
+  document.writeln(' INPUTED : ',string);
+  document.writeln('<br> Char at (0): ',string.charAt(0));
+  document.writeln('<br> CharCode at (0): ',string.charCodeAt(0));
+  document.writeln('<br> CharCode at (1): ',string.charCodeAt(1));
+  document.writeln('<br> Slice (1,3): ',string.slice(1,3));
+  document.writeln('<br> Substr (0,7) : ',string.substr(0,7));
+  document.writeln('<br> Substring (0,3): ',string.substring(0,3));
+  document.writeln('<br> Search+RegExp(/"hello"/i): ',string.match(RegExp(/hello/ig)));
+  var a=string.match(RegExp(/hello/ig));
+  document.writeln('<br> Variable?(/"hello"/i): ',a);
 }
