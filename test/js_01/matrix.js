1d3dac022de77806a1520de50690d006fbe1649f
Vlad <brainacadem@gmail.com>
2016-06-14 19:52:16 +0300

    add matrix

---
+++ test/js_01/matrix.js
@@ -0,0 +1,39 @@
+var mul = document.getElementById('multiplication-matrix');
+var setTable = document.getElementById('set-matrix');
+
+setTable.innerHTML = render(setMatrix(10));
+mul.innerHTML = render(setMulMatrix(10));
+
+
+function setMatrix(size) {
+	var array = [];
+	for (var i = 0; i < size; i++) {
+		array[i] = [];
+		for (var j = 0; j < size; j++) {
+			array[i][j] = j + 1;
+		}
+	}
+	return array;
+}
+
+function setMulMatrix(size) {
+	var array = [];
+	for (var i = 0; i < size; i++) {
+		array[i] = [];
+		for (var j = 0; j < size; j++) {
+			array[i][j] = (j+1)*(i+1);
+		}
+	}
+	return array;
+}
+
+function render(array) {
+    var rowsQty = array.length;
+    var result = [];
+    for (var i = 0; i < rowsQty; i++) {
+        var row = ['<tr><td>', array[i].join('</td><td>'), '</td></tr>'].join('');
+        result.push(row);
+    }
+
+    return result.join('');
+}
