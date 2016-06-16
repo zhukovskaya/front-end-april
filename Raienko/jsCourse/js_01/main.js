b1c9f780405bf0e529d4bd1fe7d419c9099716bb
Raienko_Notebook <raienko@gmail.com>
2016-06-16 12:41:08 +0300

    some shit

--- Raienko/jsCourse/js_01/main.js
+++ Raienko/jsCourse/js_01/main.js
@@ -1,45 +1,3 @@
-<<<<<<< HEAD
-function cloneArray(array) {
-    // if (!isArray(array)) {
-    // 	return throw new Error("Enter an array!")
-    // }
-    // var clone = [],
-    //     i = 0,
-    //     len = array.length;
-
-    // for (; i < len; i++) {
-    //     if (array[i].length && array[i].constructor === Array) {
-    //         clone[i] = cloneArray(array[i])
-    //     } else {
-    //         clone[i] = array[i];
-    //     }
-    // }
-
-    // var clone = array.concat([]);
-    // return clone;
-    return array.slice();
-}
-
-
-var a = [1, 2, 3, [4, 5]];
-var b = cloneArray(a);
-console.log(" a : ", a);
-console.log(" b : ", b);
-b[3].push(6);
-b.push(8);
-console.log("===== after push =====");
-console.log(" a : ", a);
-console.log(" b : ", b);
-
-
-var obj = {
-	name : "Vlad",
-	height : 190,
-	walk : function() { return "I can walk"},
-	makeCode : function() {return "I make code"}
-}
-
-=======
 (function() {
     function Calculator() {
         var first, second;
@@ -89,8 +47,3 @@ var obj = {
     calc.calculate("3 + 7"); // 10
 
 })();
-<<<<<<< HEAD
->>>>>>> da5bccb208f2047fccaabf2c3d3a7099a66278f2
-=======
->>>>>>> 7d03179b858663c16312cf2aa83ce9f1ebcb2328
->>>>>>> b13366fcebe9e5ee70e32c1712c3b53e079b0865
--- Raienko/jsCourse/js_01/matrix.js
+++ Raienko/jsCourse/js_01/matrix.js
@@ -39,7 +39,14 @@ function setBatterfly(size) {
 
 function setPascalTriangle(size) {
 	var triangle = [];
-
+  for (var i=0;i<size;i++){
+    triangle[i]=[];
+    for (var j=0;j<size;j++){
+      if (j==0||j==i) {triangle[i][j]=1;} else if (i>j){
+        triangle[i][j]=triangle[i-1][j-1]+triangle[i-1][j];
+      };
+    }
+  }
 	return triangle;
 }
 
