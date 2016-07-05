338d2976b6dad04e5b87e69a770659dd41de5bbc
Raienko <raienko.a@gmail.com>
2016-07-05 21:12:27 +0300

    update lesson 5jul

--- Raienko/jsCourse/04/main.js
+++ Raienko/jsCourse/04/main.js
@@ -1,49 +1,49 @@
 (function() {
-    function Calculator() {
-        var first, second;
-        this.read = function() {
-            first = +prompt("Enter first number : ");
-            second = +prompt("Enter second number : ");
-        }
-
-        this.sum = function() {
-            return first + second;
-        }
-
-        this.mul = function() {
-            return first * second;
-        }
+  function Calculator() {
+    var first, second;
+    this.read = function() {
+      first = +prompt("Enter first number : ");
+      second = +prompt("Enter second number : ");
     }
 
-    function Accumulator(startingValue) {
-        this.value = startingValue;
-
-        this.read = function() {
-            this.value += +prompt("Enter value")
-        }
+    this.sum = function() {
+      return first + second;
     }
 
-    function Calculator() {
+    this.mul = function() {
+      return first * second;
+    }
+  }
 
-        var obj = {
-            '+': function(a, b) {
-                return a + b;
-            },
-            '-': function(a, b) {
-                return a - b;
-            }
-        }
+  function Accumulator(startingValue) {
+    this.value = startingValue;
 
-        this.calculate = function(string) {
-            var str = string.split(' '),
-                number1 = str[0],
-                operator = str[1],
-                number2 = str[2];
-            obj[operator](+number1, +number2)
-        }
+    this.read = function() {
+      this.value += +prompt("Enter value")
+    }
+  }
+
+  function Calculator() {
+
+    var obj = {
+      '+': function(a, b) {
+        return a + b;
+      },
+      '-': function(a, b) {
+        return a - b;
+      }
     }
 
-    var calc = new Calculator;
-    calc.calculate("3 + 7"); // 10
+    this.calculate = function(string) {
+      var str = string.split(' '),
+        number1 = str[0],
+        operator = str[1],
+        number2 = str[2];
+      obj[operator](+number1, +number2)
+    }
+  }
+
+  var calc = new Calculator;
+  calc.calculate("3 + 7"); // 10
 
 })();
