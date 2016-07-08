09c9e140bac4042b64f8d668044dca17fbd9e47e
DivaSerg <divaserg@gmail.com>
2016-07-08 21:56:48 +0300

    8-07

--- Dibrova/tasks/07_javascript_advanced/phone_book/main.js
+++ Dibrova/tasks/07_javascript_advanced/phone_book/main.js
@@ -1,9 +1,23 @@
+// for (var i = 0; i < document.getElementById('AddContact').length; i++) {
+//     console.dir(document.getElementById('AddContact')[i]);
+// }
 
 
-for (var i = 0; i < document.getElementById('add-contact').length; i++) {
-    console.dir(document.getElementById('add-contact')[i]);
+
+// name.onblur = function () {
+// 	console.log(this.value);
+// }
+
+// AddContact.addEventListener("focus", function() {
+	// console.log(name.value);
+// })
+
+
+
+var ImName = document.getElementById('name');
+ 
+console.log(ImName);
+
+ImName.oninput = function () {
+	console.log(ImName.value);
 }
-
-
-
-// console.log(document.getElementById('add-contact'));
