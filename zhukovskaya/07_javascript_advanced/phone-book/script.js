ece91491f6b5f85db2c0a6741b10ecebe05a3dfe
zhukovskaya <zhukovskay.e.v@gmail.com>
2016-07-09 07:38:45 +0300

    add and delete in phonbook

--- zhukovskaya/07_javascript_advanced/phone-book/script.js
+++ zhukovskaya/07_javascript_advanced/phone-book/script.js
@@ -1,47 +1,89 @@
-window.onload = function(){ 
+ window.onload = function(){ 
+// document.addEventListener( "DOMContentLoaded", function() {
+//   document.removeEventListener( "DOMContentLoaded", arguments.callee, false );
+
+
    var doc = document,
-	name = doc.getElementById("name"),
+	name = doc.getElementById("user-name"),
 	phone = doc.getElementById("phone"),
 	email = doc.getElementById("email"),
 	buttonAdd = doc.getElementById("add"),
-	phoneRegExp = /^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im,
-	emailRegExp = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
-	form = doc.getElementById("info");
+	phoneRegExp = /\d/,
+	emailRegExp =/\w/,
+	errorMessage = doc.getElementById("error"),
+	form = doc.getElementById("info"),
+	table = doc.getElementById("myTable");
 
-form.onclick = function() {
-    
-	if(!validError) {
-		alert('hi');
-	}	//createElement()
 
-};	
+	buttonAdd.onclick = function add() {
+		
+		
+		var newRow = table.insertRow(-1);
+		var cellName = newRow.insertCell(-1);
+		cellName.innerHTML = name.value;
+		var cellPhone = newRow.insertCell(-1);
+		cellPhone.innerHTML = phone.value;
+		var cellEmail = newRow.insertCell(-1);
+		cellEmail.innerHTML = email.value;
+		var cellDelete = newRow.insertCell(-1);
+		cellDelete.innerHTML = "<a>Delete</a>";
+		// var a = doc.createElement("a");
+		// cellDelete.appendChild(a);
+		// a.innerHTML = "Delete";
+
+		 var links = doc.getElementsByTagName("a");
+
+		 for (var i = 0; i < links.length; i++) {
+		 var link = links[i];
+		    		
+		    link.onclick = function() {
+		    	//alert("hi");
+		    	var el = link.parentNode.parentNode;
+       			el.parentNode.removeChild(el);
+		     	}
+		   }
+		}
+	// if(isValid ()) {
+	// 	alert('hi');
+	// }	
+		
+		
 
 
 //validation
 
-	function validName() {
-		return name && name.length;
+	// function validName() {
+	// 	return name && name.length;
 		
-	}
+	// }
 
-	function validPhone() {
-		return phoneRegExp.test(phone.value); 
-	}
+	// function validPhone() {
+	// 	return phoneRegExp.test(phone.value); 
+	// }
 
-	function validEmail(email) {
-	 	return emailRegExp.test(email.value);
-	}
+	// function validEmail() {
+	//  	return emailRegExp.test(email.value);
+	// }
 
-	function validError() {
-		if(!validName() || !validPhone() || !validEmail()) {
-		var error = doc.createElement("p");
-		error.innerHTML = "Please, enter valid information" ;
-		form.appendChild(error);
-		}
-	}
+	// function isValid() {
+		 
+	// 	if(!validName() || !validPhone() || !validEmail()) {
+	// 		errorMessage.innerHTML = "Please, enter valid information" ;
+	// 	} else {
+	// 		return validName() && validPhone() && validEmail();
+	// 	}
+	// }
+	
+	 // function () {
+	 // 	var obj = {};
+	 // 	obj.name = 
+
 	 
-};
 
+	
 
+///^[\+]?[(]?[0-9]{3}[)]?[-\s\.]?[0-9]{3}[-\s\.]?[0-9]{4,6}$/im
+// /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
 
-
+// }, false );
+}
