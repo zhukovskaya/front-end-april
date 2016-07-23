1b1772407b98f073400266ab21a68d0112786786
TkachenkoArtem <igrfy87@gmail.com>
2016-07-24 00:08:38 +0300

    fixed media, started adding hamburger

--- Tkachenko/02_La_Casa/app/scripts/script.js
+++ Tkachenko/02_La_Casa/app/scripts/script.js
@@ -1,39 +1,54 @@
 /*BACK-TO-TOP*/
 
-if ($('#back-to-top').length) {
+(function() {
+  if ($('#back-to-top').length) {
     var scrollTrigger = 100, // px
-        backToTop = function() {
-            var scrollTop = $(window).scrollTop();
-            if (scrollTop > scrollTrigger) {
-                $('#back-to-top').addClass('show');
-            } else {
-                $('#back-to-top').removeClass('show');
-            }
-        };
+      backToTop = function() {
+        var scrollTop = $(window).scrollTop();
+        if (scrollTop > scrollTrigger) {
+          $('#back-to-top').addClass('show');
+        } else {
+          $('#back-to-top').removeClass('show');
+        }
+      };
 
     backToTop();
     $(window).on('scroll', function() {
-        backToTop();
+      backToTop();
     });
     $('#back-to-top').on('click', function(e) {
-        e.preventDefault();
-        $('html,body').animate({
-            scrollTop: 0
-        }, 700);
+      e.preventDefault();
+      $('html,body').animate({
+        scrollTop: 0
+      }, 700);
     });
-}
+  }
+})();
 
+/*SPINER MENU*/
 
-// ____________________________________________________________________
+(function() {
+  var counter = 0;
 
-var counter = 0;
-
-$('.fa-cog').click(function(clickmenu) {
-    clickmenu.preventDefault();
+  $('.fa-cog').click(function(e) {
+    e.preventDefault();
     counter++;
-    if((counter%2) == 0) {
+    if ((counter % 2) === 0) {
       $('main div.mainmenu div.mainmenu__list').removeClass('nohidden');
     } else {
       $('main div.mainmenu div.mainmenu__list').addClass('nohidden');
+      $('html,body').animate({
+        scrollTop: $('main').position().top
+      }, 700);
     }
-});
+  });
+})();
+
+/*BARS*/
+(function() {
+  if ($(window).width() < 768) {
+    console.log($(window).width());
+    $('nav.headertop__right').addClass('headertop__right--hamburger fa fa-bars');
+    $('nav.headertop__right').addClass('hidden');
+  } else  $('nav.headertop__right').removeClass('hidden');
+})();
